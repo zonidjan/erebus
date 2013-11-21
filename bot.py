ab7c0c0bba06ff874eaf1bf35c8575e55b29709d
BiohZn <biohzn@mustis.org>
2013-11-21 12:42:51 -0600

    - Fixed

--- bot.py
+++ bot.py
@@ -19,26 +19,33 @@ class Bot(object):
 	def getdata(self):
 		for line in self.conn.read():
 			print self.nick, '[I]', line
+
 			if not self.conn.registered():
 				pieces = line.split()
+
 				if pieces[0] == "PING":
 					self.conn.send("PONG %s" % (pieces[1]))
+
 				elif pieces[1] == "001":
 					self.conn.registered(True)
+
 					for c in self.chans:
 						self.join(c)
 			else:
 				self.parse(line)
 	def parse(self, line):
 		pieces = line.split()
+
 		if pieces[1] == "PRIVMSG":
 			nick = pieces[0].split('!')[0][1:]
 			user = self.parent.user(nick)
 			chan = self.parent.channel(pieces[2])
 			msg = ' '.join(pieces[3:])[1:]
 			self.parsemsg(user, chan, msg)
+
 		elif pieces[0] == "PING":
 			self.conn.send("PONG %s" % (pieces[1]))
+
 		elif pieces[1] == "JOIN":
 			nick = pieces[0].split('!')[0][1:]
 			user = self.parent.user(nick)
@@ -47,18 +54,23 @@ class Bot(object):
 	def parsemsg(self, user, chan, msg):
 		if msg[0] == '!': #TODO check config for trigger
 			msg = msg[1:]
+
 		else:
 			return
+
 		pieces = msg.split()
 		cmd = pieces[0].upper()
+
 		if cmd == "EVAL":
 			try: ret = eval(' '.join(pieces[1:]))
 			except: self.msg(chan, "Error: %s %s" % (sys.exc_info()[0], sys.exc_info()[1]))
 			else: self.msg(chan, "Done: %r" % (ret))
+
 		elif cmd == "EXEC":
 			try: exec ' '.join(pieces[1:])
 			except: self.msg(chan, "Error: %s %s" % (sys.exc_info()[0], sys.exc_info()[1]))
 			else: self.msg(chan, "Done.")
+
 		#TODO
 
 	def msg(self, target, msg):
@@ -68,10 +80,13 @@ class Bot(object):
 			if target[0] == '#': self.conn.send("PRIVMSG %s :%s" % (target, msg))
 			else: self.conn.send("NOTICE %s :%s" % (target, msg))
 		else: raise TypeError('Bot.msg() "target" must be Erebus.User, Erebus.Channel, or string')
+
 	def join(self, chan):
 		self.conn.send("JOIN %s" % (chan))
+
 	def part(self, chan):
 		self.conn.send("PART %s" % (chan))
+
 	def quit(self, reason="Shutdown"):
 		self.conn.send("QUIT :%s" % (reason))
 
@@ -107,13 +122,17 @@ class BotConnection(object):
 	def send(self, line):
 		print self.nick, '[O]', line
 		self.write(line)
+
 	def write(self, line):
 		self.socket.sendall(line+"\r\n")
+
 	def read(self):
 		self.buffer += self.socket.recv(8192)
 		lines = []
+
 		while '\r\n' in self.buffer:
 			pieces = self.buffer.split('\r\n', 1)
 			lines.append(pieces[0])
 			self.buffer = pieces[1]
+
 		return lines
