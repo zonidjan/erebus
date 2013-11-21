9c9352945379890e03850ab62c5949f9cb082363
John Runyon <jayru@users.noreply.github.com>
2013-11-21 12:41:46 -0600

    Updates

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 
 #TODO: error checking
 
-import socket
+import socket, sys
 
 #bots = {'erebus': bot.Bot(nick='Erebus', user='erebus', bind='', server='irc.quakenet.org', port=6667, realname='Erebus')}
 class Bot(object):
@@ -13,18 +13,67 @@ class Bot(object):
 
 		self.conn = BotConnection(self, nick, user, bind, server, port, realname)
 	def connect(self):
-		self.conn.connect()
+		if self.conn.connect():
+			self.parent.newfd(self, self.conn.socket.fileno())
+
 	def getdata(self):
 		for line in self.conn.read():
-			self.parse(line)
+			print self.nick, '[I]', line
+			if not self.conn.registered():
+				pieces = line.split()
+				if pieces[0] == "PING":
+					self.conn.send("PONG %s" % (pieces[1]))
+				elif pieces[1] == "001":
+					self.conn.registered(True)
+					for c in self.chans:
+						self.join(c)
+			else:
+				self.parse(line)
 	def parse(self, line):
 		pieces = line.split()
-		if not self.conn.registered():
-			if pieces[0] == "PING":
-				self.conn.send("PONG %s" % (pieces[1]))
-			elif pieces[1] == "001":
-				self.conn.registered(True)
-				print "!!!REGISTERED!!!"
+		if pieces[1] == "PRIVMSG":
+			nick = pieces[0].split('!')[0][1:]
+			user = self.parent.user(nick)
+			chan = self.parent.channel(pieces[2])
+			msg = ' '.join(pieces[3:])[1:]
+			self.parsemsg(user, chan, msg)
+		elif pieces[0] == "PING":
+			self.conn.send("PONG %s" % (pieces[1]))
+		elif pieces[1] == "JOIN":
+			nick = pieces[0].split('!')[0][1:]
+			user = self.parent.user(nick)
+			chan = self.parent.channel(pieces[2]) #TODO TODO TODO
+			
+	def parsemsg(self, user, chan, msg):
+		if msg[0] == '!': #TODO check config for trigger
+			msg = msg[1:]
+		else:
+			return
+		pieces = msg.split()
+		cmd = pieces[0].upper()
+		if cmd == "EVAL":
+			try: ret = eval(' '.join(pieces[1:]))
+			except: self.msg(chan, "Error: %s %s" % (sys.exc_info()[0], sys.exc_info()[1]))
+			else: self.msg(chan, "Done: %r" % (ret))
+		elif cmd == "EXEC":
+			try: exec ' '.join(pieces[1:])
+			except: self.msg(chan, "Error: %s %s" % (sys.exc_info()[0], sys.exc_info()[1]))
+			else: self.msg(chan, "Done.")
+		#TODO
+
+	def msg(self, target, msg):
+		if isinstance(target, self.parent.User): self.conn.send("NOTICE %s :%s" % (target.nick, msg))
+		elif isinstance(target, self.parent.Channel): self.conn.send("PRIVMSG %s :%s" % (target.name, msg))
+		elif isinstance(target, basestring):
+			if target[0] == '#': self.conn.send("PRIVMSG %s :%s" % (target, msg))
+			else: self.conn.send("NOTICE %s :%s" % (target, msg))
+		else: raise TypeError('Bot.msg() "target" must be Erebus.User, Erebus.Channel, or string')
+	def join(self, chan):
+		self.conn.send("JOIN %s" % (chan))
+	def part(self, chan):
+		self.conn.send("PART %s" % (chan))
+	def quit(self, reason="Shutdown"):
+		self.conn.send("QUIT :%s" % (reason))
 
 class BotConnection(object):
 	state = 0 # 0=disconnected, 1=registering, 2=connected
@@ -48,29 +97,23 @@ class BotConnection(object):
 		self.send("NICK %s" % (self.nick))
 		self.send("USER %s 0 * :%s" % (self.user, self.realname))
 		self.state = 1
+		return True
 
 	def registered(self, done=False):
 		if done: self.state = 2
 		return self.state == 2
 
-	def fileno(self):
-		if self.socket is not None:
-			return self.socket.fileno()
-		else:
-			return None
-
 	#TODO: rewrite send() to queue
 	def send(self, line):
+		print self.nick, '[O]', line
 		self.write(line)
 	def write(self, line):
-		print self.nick, '[O]', line
 		self.socket.sendall(line+"\r\n")
 	def read(self):
 		self.buffer += self.socket.recv(8192)
 		lines = []
 		while '\r\n' in self.buffer:
 			pieces = self.buffer.split('\r\n', 1)
-			print self.nick, '[I]', pieces[0]
 			lines.append(pieces[0])
 			self.buffer = pieces[1]
 		return lines
--- erebus.py
+++ erebus.py
@@ -6,28 +6,105 @@ import sys, select
 import bot
 
 class Erebus(object):
-	pass
+	bots = {}
+	fds = {}
+	mods = {}
+	msghandlers = {}
+
+	class User(object):
+		chans = []
+		def __init__(self, nick, auth=None):
+			self.nick = nick
+			self.auth = auth
+			if auth is not None:
+				self.checklevel()
+		def authed(self, auth):
+			self.auth = auth
+			self.checklevel()
+		def checklevel(self): self.level = 9999 #TODO get level from db
+		def __str__(self): return self.nick
+		def __repr__(self): return "<User %r>" % (self.nick)
+	class Channel(object):
+		users = []
+		voices = []
+		ops = []
+		def __init__(self, name):
+			self.name = name
+		def userjoin(self, user, level=None):
+			if user not in self.users: self.users.append(user)
+			if level == 'op' and user not in self.ops: self.ops.append(user)
+			if level == 'voice' and user not in self.voices: self.voices.append(user)
+		def userpart(self, user):
+			if user in self.ops: self.ops.remove(user)
+			if user in self.voices: self.voices.remove(user)
+			if user in self.users: self.users.remove(user)
+		def userop(self, user):
+			if user in self.users and user not in self.ops: self.ops.append(user)
+		def uservoice(self, user):
+			if user in self.users and user not in self.voices: self.voices.append(user)
+		def userdeop(self, user):
+			if user in self.ops: self.ops.remove(user)
+		def userdevoice(self, user):
+			if user in self.voices: self.voices.remove(user)
+
+		def __str__(self): return self.name
+		def __repr__(self): return "<Channel %r>" % (self.name)
+
+	def __init__(self):
+		self.po = select.poll()
+
+	def newbot(self, nick, user, bind, server, port, realname, chans):
+		if bind is None: bind = ''
+		obj = bot.Bot(self, nick, user, bind, server, port, realname, chans)
+		self.bots[nick.lower()] = obj
+	def newfd(self, obj, fileno):
+		print "newfd(Erebus(), %r, %r)" % (obj, fileno)
+		self.fds[fileno] = obj
+		self.po.register(fileno, select.POLLIN)
+	def bot(self, name):
+		return self.bots[name.lower()]
+	def fd(self, fileno):
+		return self.fds[fileno]
+
+	def user(self, nick): #TODO
+		return self.User(nick.lower())
+	def channel(self, name): #TODO
+		return self.Channel(name.lower())
+
+	def poll(self):
+		return self.po.poll(60000)
+
+	def connectall(self):
+		for bot in self.bots.itervalues():
+			if bot.conn.state == 0:
+				bot.connect()
+
+	#module functions
+	def modlist(self): pass
+	def hasmod(self, name): pass
+	def loadmod(self, name): pass
+	def unloadmod(self, name): pass
+	def reloadmod(self, name): pass
+
+	#bind functions
+	def bind(self, word, handler): pass
+	def addbind(self, word, handler): pass
+	def rmbind(self, word, handler): pass
+	def getbind(self, word, handler): pass
+
 
 main = Erebus()
-bots = {}
-fds = {}
-po = select.poll()
 
 def setup():
 	global bots, fds
-	bots = {'erebus': bot.Bot(main, 'Erebus', 'erebus', '', 'irc.quakenet.org', 6667, 'Erebus', [])}
-	fds = {}
-
-	bots['erebus'].connect()
-	fds[bots['erebus'].conn.fileno()] = bots['erebus']
-	po.register(bots['erebus'].conn.fileno(), select.POLLIN)
+	main.newbot('Erebus', 'erebus', None, 'irc.quakenet.org', 6667, 'Erebus', ['#dimetest'])
+	main.bot('erebus').connect()
 
 def loop():
-	poready = po.poll(60000)
-	for (fd,mask) in poready:
-		fds[fd].getdata()
+	poready = main.poll()
+	for (fileno,mask) in poready:
+		main.fd(fileno).getdata()
 
 if __name__ == '__main__':
 	setup()
-	while True:
-		loop()
+	while True: loop()
