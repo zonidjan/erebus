88eb6c820a3504b4310b3fe1e47eedb60cccb5a7
Conny Sjöblom <biohzn@mustis.org>
2014-02-07 01:05:29 +0200

    Modified coins.py, committing what i have atm

--- modules/coins.py
+++ modules/coins.py
@@ -17,8 +17,13 @@ modstart = lib.modstart
 modstop = lib.modstop
 
 # module code
+import re
 import json
 import requests
+
+coin_regex = (
+  re.compile(r'([0-9., ]+[0-9]+) (BTC|bitcoin|doge|dogecoin|ltc|litecoin)'), # Fix regex
+)
 
 url = 'http://www.cryptocoincharts.info/v2/api/tradingPairs'
 
@@ -71,3 +76,41 @@ def cmd_gtest(bot, user, chan, realtarget, *args):
 		btc_eur = get_coin_price('btc_eur')
 		price = str(float(ltc_btc[0]['price']) * float(btc_eur[0]['price']))
 		bot.msg(chan, "1 LTC = %s EUR" % price)
+
+@lib.hooknum("PRIVMSG")
+def privmsg_hook(bot, line):
+
+	try:
+		linetx = line.split(None, 3)[3][1:]
+	except IndexError:
+		linetx = ''
+
+	chan = line.split()[2]
+
+	if 'btc' in line or 'bitcoin' in line or 'doge' in line or 'dogecoin' in line:
+		for r in coin_regex:
+			for amount, coin in r.findall(linetx):
+				amount = amount.replace(" ", "")
+				if 'btc' in coin or 'bitcoin' in coin:
+					try:
+						response = get_coin_price('btc_eur')
+						price = str(float(response[0]['price']) * float(amount))
+						bot.msg(chan, "%s BTC = %s EUR" % (amount, price))
+					except:
+						pass
+
+				if 'ltc' in coin or 'litecoin' in coin:
+					pass	# Add LTC
+
+				if 'doge' in coin or 'dogecoin' in coin:
+					try:
+						doge_btc = get_coin_price('doge_btc')
+						btc_eur = get_coin_price('btc_eur')
+						price = str(float(doge_btc[0]['price']) * float(btc_eur[0]['price']) * float(amount))
+						bot.msg(chan, "%s DOGE = %s EUR" % (amount, price))
+					except:
+						bot.msg(chan, "Invalid amount.")
+						
+					
+				print amount
+				print coin
