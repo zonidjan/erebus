390fbad48dc38d92c4c2c7db89beab407b4c3ad8
Conny Sjöblom <biohzn@mustis.org>
2014-02-20 00:35:03 +0200

    urls.py module redone

--- modules/urls.py
+++ modules/urls.py
@@ -19,20 +19,22 @@ modstart = lib.modstart
 modstop = lib.modstop
 
 # module code
-import re, json, urllib2, urlparse, HTMLParser
+import re, urllib2, urlparse, json, HTMLParser
 from BeautifulSoup import BeautifulSoup
 
-hostmask_regex = re.compile(r'^(.*)!(.*)@(.*)$')
+html_parser = HTMLParser.HTMLParser()
 
+hostmask_regex = re.compile(r'^(.*)!(.*)@(.*)$')
+url_regex = re.compile(r'((?:https?://|spotify:)[^\s]+)')
 spotify_regex = (
 	re.compile(r'spotify:(?P<type>\w+):(?P<track_id>\w{22})'),
-	re.compile(r'http://open.spotify.com/(?P<type>\w+)/(?P<track_id>\w{22})')
+	re.compile(r'https?://open.spotify.com/(?P<type>\w+)/(?P<track_id>\w{22})')
 )
 youtube_regex = (
 	re.compile(r'https?://(?:www\.)?youtube\.com/watch\?[a-zA-Z0-9=&_\-]+'),
 )
 twitch_regex = (
-	re.compile('(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?'), #TODO
+	re.compile(r'https?://(?:www\.)?twitch.tv/(.*)\w{1,}'),
 )
 
 def parser_hostmask(hostmask):
@@ -60,38 +62,45 @@ def parser_hostmask(hostmask):
 	}
 
 @lib.hooknum("PRIVMSG")
-def privmsg_hook(bot, line):
-	sender = parser_hostmask(line[1:line.find(' ')])
+def privmsg_hook(bot, textline):
+	user = parser_hostmask(textline[1:textline.find(' ')])
+	chan = textline.split()[2]
 
 	try:
-		linetx = line.split(None, 3)[3][1:]
+		line = textline.split(None, 3)[3][1:]
 	except IndexError:
-		linetx = ''
+		line = ''
 
-	chan = line.split()[2]
+	for match in url_regex.findall(line):
+		if match:
+			print match
+			if 'open.spotify.com' in match or 'spotify:' in match:
+				for r in spotify_regex:
+					for sptype, track in r.findall(match):
+						bot.msg(chan, unescape(gotspotify(sptype, track)))
 
-	if 'open.spotify.com' in line or 'spotify:' in line:
-		for r in spotify_regex:
-			for sptype, track in r.findall(linetx):
-				bot.msg(chan, gotspotify(sptype, track))
+			elif 'youtube.com' in match or 'youtu.be' in match:
+				for r in youtube_regex:
+					for url in r.findall(match):
+						bot.msg(chan, unescape(gotyoutube(url)))
 
-	elif 'youtube.com' in line or 'youtu.be' in line:
-		print "got youtube!"
-		for r in youtube_regex:
-			for url in r.findall(linetx):
-				bot.msg(chan, gotyoutube(url))
+			elif 'twitch.tv' in match:
+				for r in twitch_regex:
+					for uri in r.findall(match):
+						bot.msg(chan, unescape(gottwitch(uri)))
 
-	elif 'twitch.tv' in line: pass #TODO fix twitch
+			else:
+				bot.msg(chan, unescape(goturl(match)))
 
-	else: pass #TODO generic <title> checker
-
+def unescape(line):
+	return html_parser.unescape(line)
 
 def gotspotify(type, track):
 	url = 'http://ws.spotify.com/lookup/1/?uri=spotify:%s:%s' % (type, track)
 	xml = urllib2.urlopen(url).read()
-	soup = BeautifulSoup(xml)
+	soup = BeautifulSoup(xml, convertEntities=BeautifulSoup.HTML_ENTITIES)
 	lookup_type = soup.contents[2].name
-		
+
 	if lookup_type == 'track':
 		name = soup.find('name').string
 		album_name = soup.find('album').find('name').string
@@ -102,15 +111,15 @@ def gotspotify(type, track):
 		length = float(soup.find('length').string)
 		minutes = int(length)/60
 		seconds =  int(length)%60
-	
+
 		return 'Track: %s - %s / %s %s:%.2d %2d%%' % (artist_name, name, album_name, minutes, seconds, popularity)
-	
+
 	elif lookup_type == 'album':
 		album_name = soup.find('album').find('name').string
 		artist_name = soup.find('artist').find('name').string
 		released = soup.find('released').string
 		return 'Album: %s - %s - %s' % (artist_name, album_name, released)
-	
+
 	else:
 		return 'Unsupported type.'
 
@@ -130,25 +139,15 @@ def gotyoutube(url):
 	except:
 		pass
 
+def gottwitch(uri):
+		url = 'http://api.justin.tv/api/stream/list.json?channel=%s' % uri.split('/')[0]
+		respdata = urllib2.urlopen(url).read()
+		twitch = json.loads(respdata)
+		try:
+			return 'Twitch: %s (%s playing %s)' % (twitch[0]['channel']['status'], twitch[0]['channel']['login'], twitch[0]['channel']['meta_game'])
+		except:
+			return 'Twitch: Channel offline.'
 
-def gottwitch(url):
-	return ""
-	#FIXME:
-	try:
-		linetx = line.split(None, 3)[3][1:]
-	except IndexError:
-		linetx = ''
-
-	if checkfor not in line:
-		return # doesn't concern us
-
-	for p, h, c in url_regex.findall(linetx):
-		if checkfor in h:
-			url = 'http://api.justin.tv/api/stream/list.json?channel=%s' % c[1:]
-			respdata = urllib2.urlopen(url).read()
-			twitch = json.loads(respdata)
-			try:
-				bot.msg(line.split()[2], 'Twitch: %s (%s playing %s)' % (twitch[0]['channel']['status'], twitch[0]['channel']['login'], twitch[0]['channel']['meta_game']))
-			except:
-				bot.msg(line.split()[2], 'Twitch: Channel offline.')
-
+def goturl(url):
+	soup = BeautifulSoup(urllib2.urlopen(url))
+	return soup.title.string
