7c4a7d0ac3865f38f18c91ef6725c1408195c387
zonidjan <git@jfr.im>
2018-07-30 00:07:52 -0500

    stafflist - add online nicks

--- modules/stafflist.py
+++ modules/stafflist.py
@@ -18,17 +18,24 @@ modstart = lib.modstart
 modstop = lib.modstop
 
 # module code
+def _getnicks(auth):
+	nicks = [x.nick for x in lib.parent.getuserbyauth(auth)]
+	if len(nicks) == 0:
+		return 'not online'
+	else:
+		return ', '.join(nicks)
+
 @lib.hook(needchan=False)
 @lib.help(None, 'lists staff')
 @lib.argsEQ(0)
 def stafflist(bot, user, chan, realtarget, *args):
-	c = lib.parent.query("SELECT auth, level FROM users WHERE level > %s", (lib.parent.cfg.get('stafflist', 'minstafflevel', default=lib.KNOWN)))
+	c = lib.parent.query("SELECT auth, level FROM users WHERE level > %s", (lib.parent.cfg.get('stafflist', 'minstafflevel', default=lib.KNOWN),))
 	if c:
 		staffs = c.fetchall()
 		c.close()
 		if len(staffs) > 0:
 			if user.glevel > lib.KNOWN:
-				response = ["%s (%s)" % (i['auth'], i['level']) for i in staffs]
+				response = ["#%s (%s, %s)" % (i['auth'], _getnicks(i['auth']), i['level']) for i in staffs]
 			else:
-				response = [i['auth'] for i in staffs]
+				response = ["#%s (%s)" % (i['auth'], _getnicks(i['auth'])) for i in staffs]
 			user.msg("Staff listing: %s" % (', '.join(response)))
